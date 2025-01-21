f3d515983149360e41f24e129ddfdfbebd476ebc
hariprasad-sundaresan-bjsscloud-net <hariprasad.sundaresan@bjsscloud.net>
2025-01-21 21:37:14 +0000

    Initial Version - POC

--- infra/gcp/api.tf
+++ infra/gcp/api.tf
@@ -1,11 +1,14 @@
-# # Enable required APIs (if not already enabled in another file)
-# resource "google_project_service" "cloud_apis" {
-#   for_each = toset([
-#     "cloudfunctions.googleapis.com",
-#     "cloudbuild.googleapis.com",
-#     "storage.googleapis.com"
-#   ])
-#   project = var.project_id
-#   service = each.key
-# }
-
+# Enable required APIs (if not already enabled in another file)
+resource "google_project_service" "apis" {
+  for_each = toset([
+    "cloudbuild.googleapis.com",
+    "cloudfunctions.googleapis.com",    
+    "storage.googleapis.com",           
+    "logging.googleapis.com",           
+    "monitoring.googleapis.com",        
+    "iam.googleapis.com",
+    # "iap.googleapis.com", 
+  ])
+  project = var.project_id
+  service = each.key
+}
